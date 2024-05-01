e61ebf50c74f58ef5fd89c1ec783beee938e2c13
Aneesh Sharma <aneeshsharma1024@gmail.com>
2024-05-01 23:41:28 +0530

    Adding className prop for Button component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,14 +15,14 @@ const Home: React.FC<Props> = (props) => {
     <div className="w-full">
       <main className="w-full flex flex-col gap-10 text-zinc-600">
         <section className="relative w-full flex items-center justify-center py-14">
-          <div className="absolute top-0 w-full h-1/2 bg-red-400 -z-10"></div>
+          <div className="absolute top-0 w-full h-1/ -z-10"></div>
           <div className="flex flex-col w-1/2 rounded-full overflow-hidden px-10 py-4 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] bg-white">
             <div className="w-full ">
               <div className="flex items-center justify-between px-5 text-[1.1rem]">
                 <div
                   className={`font-bold cursor-pointer ${
                     current === "buy" && "border-b-4 border-b-orange-500"
-                  } py-3 px-2`}
+                  } py-4 px-2`}
                   onClick={() => onTabChange("buy")}
                 >
                   Buy
@@ -30,7 +30,7 @@ const Home: React.FC<Props> = (props) => {
                 <div
                   className={`font-bold cursor-pointer ${
                     current === "rent" && "border-b-4 border-b-orange-500"
-                  } py-3 px-2`}
+                  } py-4 px-2`}
                   onClick={() => onTabChange("rent")}
                 >
                   Rent
@@ -38,7 +38,7 @@ const Home: React.FC<Props> = (props) => {
                 <div
                   className={`font-bold cursor-pointer ${
                     current === "pg" && "border-b-4 border-b-orange-500"
-                  } py-3 px-2`}
+                  } py-4 px-2`}
                   onClick={() => onTabChange("pg")}
                 >
                   PG
@@ -46,7 +46,7 @@ const Home: React.FC<Props> = (props) => {
               </div>
             </div>
             <hr className="border border-zinc-200" />
-            <div className="w-full py-3">
+            <div className="w-full py-2">
               <div className="w-full">
                 <div className="w-full flex items-center justify-center gap-4 px-4">
                   <SearchIcon size={30} />
@@ -63,7 +63,11 @@ const Home: React.FC<Props> = (props) => {
                     </button>
                   </div>
                   <div>
-                    <Button title="Search" onclick={() => {}} className="px-5" />
+                    <Button
+                      title="Search"
+                      onclick={() => {}}
+                      className="px-5 py-2"
+                    />
                   </div>
                 </div>
               </div>
