30b3b27c123835649dfe21208c88e7572400fd6d
Aneesh Sharma <aneeshsharma1024@gmail.com>
2024-05-02 22:48:08 +0530

    Responsive Navbar

--- src/assets/Icons.tsx
+++ src/assets/Icons.tsx
@@ -153,3 +153,23 @@ export const LocateIcon = ({ size = 46, color = "currentColor", ...props }) => (
     <path d="M13 4.069V2h-2v2.069A8.01 8.01 0 0 0 4.069 11H2v2h2.069A8.008 8.008 0 0 0 11 19.931V22h2v-2.069A8.007 8.007 0 0 0 19.931 13H22v-2h-2.069A8.008 8.008 0 0 0 13 4.069ZM12 18c-3.309 0-6-2.691-6-6s2.691-6 6-6 6 2.691 6 6-2.691 6-6 6Z" />
   </svg>
 );
+
+export const MenuIcon = ({ isNavActive = false }) => (
+  <div className="flex flex-col gap-[0.36rem]">
+    <div
+      className={`h-[0.2rem] w-6 rounded-full bg-slate-600 duration-300 origin-left ${
+        isNavActive ? "rotate-45" : "rotate-0"
+      }`}
+    ></div>
+    <div
+      className={`h-[0.2rem] w-6 rounded-full bg-slate-600 duration-300 origin-center ${
+        isNavActive ? "translate-x-10 opacity-0" : "translate-x-0 opacity-100"
+      }`}
+    ></div>
+    <div
+      className={`h-[0.2rem] w-6 rounded-full bg-slate-600 duration-300 origin-left ${
+        isNavActive ? "-rotate-45" : "rotate-0"
+      }`}
+    ></div>
+  </div>
+);
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,40 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { ListIcon, UserIcon } from "../assets/Icons";
+import { ListIcon, MenuIcon, UserIcon } from "../assets/Icons";
 
 type Props = {};
 
 const Navbar: React.FC<Props> = (props) => {
+  const [isNavActive, setIsNavActive] = useState<boolean>(false);
+
   return (
-    <section className="">
-      <div className="flex flex-col md:flex-row items-center justify-between text-[0.9rem]">
-        <div>
-          <div>Logo</div>
-        </div>
-        <nav>
-          <ul className="flex items-center justify-center gap-10">
-            <li>
-              <NavLink to={"/"}>For Buyers</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/owner-dashboard"}>For Owners</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/price-trends"}>Price Trends</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/about"}>About Us</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <div>
-          <div className="flex items-center justify-center gap-2 px-4 py-2 rounded-full border border-zinc-300 duration-200 hover:shadow-[0_5px_10px_rgba(0,0,0,0.3)] cursor-pointer">
-            <ListIcon color="#7a7a7a" size={25} strokeWidth={1.4} />
-            <UserIcon color="#7a7a7a" size={30} />
+    <>
+      <div className="md:hidden flex w-full items-center justify-end z-50">
+        <button
+          className="flex items-center justify-center"
+          onClick={() => setIsNavActive(!isNavActive)}
+        >
+          <MenuIcon isNavActive={isNavActive}/>
+        </button>
+      </div>
+      <section
+        className={`fixed flex justify-end md:static w-full md:w-full duration-300 z-40 top-0 ${
+          isNavActive ? "right-0" : "-right-full"
+        }`}
+      >
+        <div className="flex flex-col md:flex-row md:items-center px-16 h-screen md:h-max justify-normal md:justify-between text-lg md:text-[0.9rem] py-8 md:p-0 duration-300 text-slate-700 font-medium bg-orange-100 md:bg-transparent w-4/5 md:w-full gap-10 md:gap-6 shadow-[-3px_5px_17px_0px_#a0aec0] md:shadow-none">
+          <div>
+            <div>Logo</div>
+          </div>
+          <nav>
+            <ul className="flex flex-col md:flex-row md:items-center justify-center gap-10">
+              <li>
+                <NavLink to={"/"}>For Buyers</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/owner-dashboard"}>For Owners</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/price-trends"}>Price Trends</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/about"}>About Us</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <div className="hidden md:block">
+            <div className="flex items-center justify-center gap-2 px-4 py-2 rounded-full border border-zinc-300 duration-200 hover:shadow-[0_5px_10px_rgba(0,0,0,0.3)] cursor-pointer">
+              <ListIcon color="#7a7a7a" size={25} strokeWidth={1.4} />
+              <UserIcon color="#7a7a7a" size={30} />
+            </div>
+          </div>
+          <div className="block md:hidden">
+            <div className="flex gap-2">
+              <NavLink
+                to={"/auth/login"}
+                className="bg-orange-400 px-4 py-2 rounded-md text-white"
+              >
+                Login
+              </NavLink>
+              <NavLink
+                to={"/auth/signup"}
+                className="bg-orange-400 px-4 py-2 rounded-md text-white "
+              >
+                Signup
+              </NavLink>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
