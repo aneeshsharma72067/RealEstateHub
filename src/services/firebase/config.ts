98d3c657a8cb7266fccf533af659f1eef82bc28e
Aneesh Sharma <aneeshsharma1024@gmail.com>
2024-05-11 22:27:12 +0530

    Re intializing firebase instance if already present

--- src/services/firebase/config.ts
+++ src/services/firebase/config.ts
@@ -1,22 +1,28 @@
-import firebase from 'firebase/app'; 
-import 'firebase/auth';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { deleteApp, getApp, getApps, initializeApp } from "firebase/app";
+import "firebase/auth";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: process.env.VITE_API_KEY,
-    authDomain: process.env.VITE_AUTH_DOMAIN,
-    projectId: process.env.VITE_PROJECT_ID,
-    storageBucket: process.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
+  apiKey: process.env.VITE_API_KEY,
+  authDomain: process.env.VITE_AUTH_DOMAIN,
+  projectId: process.env.VITE_PROJECT_ID,
+  storageBucket: process.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+let firebaseApp;
+if (!getApps().length) {
+  firebaseApp = initializeApp(firebaseConfig);
+} else {
+  firebaseApp = getApp();
+  deleteApp(firebaseApp);
+  firebaseApp = initializeApp(firebaseConfig);
+}
 const firebaseAuth = getAuth(firebaseApp);
 const firestore = getFirestore();
 const storage = getStorage(firebaseApp);
 
 export { firebaseApp, firebaseAuth, firestore, storage };
-
